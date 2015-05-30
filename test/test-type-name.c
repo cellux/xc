44dfb5da0893f4cbad4c98e3e35d2163e3b4604b
Ruzsa Balazs <cellux@omkamra.hu>
2015-05-30 22:50:32 +0200

    test-type-name

---
+++ test/test-type-name.c
@@ -0,0 +1,8 @@
+void f1(int);
+void f2(int *);
+void f3(int *[3]);
+void f4(int (*)[3]);
+void f5(int (*)[*]);
+void f6(int *());
+void f7(int (*)(void));
+void f8(int (* const [])(unsigned int, ...));
